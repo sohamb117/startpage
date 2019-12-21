2a11abd523566cd256dcdf0b61f785d7593984e7
Soham Bhattacharya <sohamb117@gmail.com>
2019-12-21 02:10:10 +0000

    Finished site

---
+++ script.js
@@ -0,0 +1,33 @@
+function formatAMPM() {
+var d = new Date(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
+    days = ['Sunday','Monday','Tuesday','Wednesday','Thuday','Friday','Saturday'];
+return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate();
+}
+
+var today = new Date();
+document.getElementById("Date").innerHTML = formatAMPM();
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('Time').innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
+startTime();
+
